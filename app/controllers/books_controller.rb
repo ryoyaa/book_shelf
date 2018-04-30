e9322178b71bd88e912a2a5435844c701c46c7ee
ryoyaa <ryoya76lima@gmail.com>
2018-04-30 17:43:30 +0900

    リファクタリング

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
   def show
     @book = Book.find(params[:id])
   end
@@ -6,29 +7,25 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-  
+
   def new
     @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
     @book.save
     redirect_to books_path
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to @book
+    redirect_to book_path
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to @book
   end
@@ -37,6 +34,10 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:name, :price, :publish, :publish_date)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#50.times do |index|
+#  Book.create(name: "本#{index}", price: "100#{index}", publish: "会社#{index}", publish_date: "201#{index}-4-10" )
+#end
+
+Faker::Config.locale = :ja
+
+100.times do |index|
+  Book.create(
+    name: Faker::Book.title,
+    price: Faker::Number.number(5),
+    publish: Faker::Book.publisher,
+    publish_date: Faker::Date.between(2.days.ago, Date.today)
+  )
+end
